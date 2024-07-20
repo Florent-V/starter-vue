ec1759375429d96918b7d376e65b178c68376120
Florent-V <vasseurflorent@gmail.com>
2024-07-20 17:03:47 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import WelKome from '@/components/WelKome.vue'
+</script>
+
+<template>
+  <main>
+    <WelKome />
+  </main>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,49 @@
+import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import nightwatchPlugin from 'vite-plugin-nightwatch'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    nightwatchPlugin(),
+    vueDevTools(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'auto',
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'vue-pwa',
+        short_name: 'vue-pwa',
+        description: 'test pwa',
+        theme_color: '#f0f0f0',
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
